9e2c1b028afe2d27044300be4c90abf99a53e491
Creator-LY <crashdiamond12@gmail.com>
2024-10-12 15:29:58 +0100

    add servo code

--- MCU/MCU.ino
+++ MCU/MCU.ino
@@ -1,13 +1,23 @@
-#define PIN_LED D12
+#include <Arduino.h>
+#include <ESP32Servo.h>
+
+
+#define PIN_SG90 13 // Output pin used
+Servo sg90;
+#include <ESP32Servo.h>
+
+
+#define PIN_SG90 13 // Output pin used
+Servo sg90;
 
 void setup() {
-  pinMode(PIN_LED, OUTPUT);
-
+  sg90.setPeriodHertz(50); // PWM frequency for SG90
+  sg90.attach(PIN_SG90); // Minimum and maximum pulse width (in µs) to go from 0° to 180
 }
 
 void loop() {
-  digitalWrite(PIN_LED, HIGH);
-  sleep(1000);
-  digitalWrite(PIN_LED, LOW);
-  sleep(1000);
+  sg90.write(90);
+  delay(500);
+  sg90.write(-90);
+  delay(500);
 }
