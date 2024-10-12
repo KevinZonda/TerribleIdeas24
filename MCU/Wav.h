2475040387b5253e2b0cfb7943c6dab76d91a97f
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2024-10-12 20:23:27 +0100

    DAC

---
+++ MCU/Wav.h
@@ -0,0 +1,23 @@
+#include <driver/dac.h>
+
+const int dacPin = 25;  // DAC1, GPIO25 (you can also use GPIO26 for DAC2)
+const int bufferSize = 1024;
+uint8_t audioBuffer[bufferSize];
+
+void init_dac() {
+  Serial.begin(115200);
+  dac_output_enable(DAC_CHANNEL_1);  // Enable DAC on GPIO25
+}
+
+void play_audio() {
+  if (Serial.available() >= bufferSize) {
+    // Read audio data from serial
+    Serial.readBytes(audioBuffer, bufferSize);
+    
+    // Output audio data to DAC
+    for (int i = 0; i < bufferSize; i++) {
+      dac_output_voltage(DAC_CHANNEL_1, audioBuffer[i]);
+      delayMicroseconds(125);  // Adjust this delay based on your sample rate
+    }
+  }
+}
