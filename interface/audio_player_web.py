1f65ac75bdb0a066a968bfc1300431343451fbc4
Shuyuan Zhang <rayzhang707@gmail.com>
2024-10-13 14:37:07 +0100

    continuous audio player web page on different lines/audio track from gpt

--- interface/audio_player_web.py
+++ interface/audio_player_web.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_mic_recorder import mic_recorder
 import base64
 import requests
 import time
@@ -15,13 +14,6 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="expanded",
 )
-
-
-if "chat_history" not in st.session_state:
-    st.session_state["chat_history"] = []
-
-if "audio" not in st.session_state:
-    st.session_state.audio = None
 
 
 def send_text_for_tts(text, url="http://127.0.0.1:11451/tts"):
@@ -40,40 +32,14 @@ def get_text_and_play_audio():
     st.audio(audio_bytes, format="audio/wav", autoplay=True)
     return gpt_resp, len(audio_bytes) / (response['sampling_rate'] * 4)
 
-    if "audio_start_time" not in st.session_state:
-        st.session_state["audio_start_time"] = time.time()
-    # Detect if the audio finishes playing
-    audio_duration = len(audio_bytes) / (response['sampling_rate'] * 4)  # Assuming 16kHz sample rate and 16-bit audio
-    st.write(f"Audio duration: {audio_duration:.2f} seconds")
-    st.session_state["chat_history"].append(
-                {"role": "assistant", "content": gpt_resp}
-            )
-    for ith_history in st.session_state["chat_history"]:
-        with st.chat_message(name=ith_history["role"]):
-            st.write(ith_history["content"])
-    while time.time() - st.session_state["audio_start_time"] < audio_duration:
-        time.sleep(1)
-    st.session_state["audio_finished"] = True
-    return True
+def update_chat_history(gpt_resp):
+    container = st.session_state["chat_container"]
+    container.write(gpt_resp)
 
-    
-
-
-# trigger the function continuously (and randomly?)
-# st.session_state["audio_finished"] = True
-# st.session_state["start"] = False
-
-# if st.button("Start"):
-#     st.session_state["start"] = True
-# if st.button("Stop"):
-#     st.session_state["start"] = False
+message_container = st.chat_message(name="assistant")
+st.session_state["chat_container"] = message_container
 
 while True:
     gpt_resp, audio_length = get_text_and_play_audio()
-    st.session_state["chat_history"].append(
-                {"role": "assistant", "content": gpt_resp}
-            )
-    for ith_history in st.session_state["chat_history"]:
-        with st.chat_message(name=ith_history["role"]):
-            st.write(ith_history["content"])
+    update_chat_history(gpt_resp)
     time.sleep(audio_length)
